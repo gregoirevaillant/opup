9a4841bb4cc83da378979e988e0afdb32a7d2b85
Grégoire VAILLANT <vaillant.g@outlook.com>
2024-10-18 10:33:03 +0200

    FEST

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-// import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
@@ -9,19 +9,19 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-	// <StrictMode>
-	<BrowserRouter>
-		<Routes>
-			<Route path="/*" element={<App />} />
-		</Routes>
-		<ToastContainer
-			position="bottom-right"
-			autoClose={3000}
-			hideProgressBar={false}
-			closeOnClick
-			rtl={false}
-			pauseOnHover
-		/>
-	</BrowserRouter>
-	// </StrictMode>
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/*" element={<App />} />
+			</Routes>
+			<ToastContainer
+				position="bottom-right"
+				autoClose={3000}
+				hideProgressBar={false}
+				closeOnClick
+				rtl={false}
+				pauseOnHover
+			/>
+		</BrowserRouter>
+	</StrictMode>
 );
